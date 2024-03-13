7f449f3109b4c7106449f97678ac798666ef06bc
hasibur14 <hasib141415@gmail.com>
2024-03-14 00:49:40 +0600

    create course reg repo

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     
+   <div>
+    <h2 className='bg-orange-600 text-7xl'>Course Registration</h2>
+   </div>
+     
+    </>
+  )
+}
+
+export default App
